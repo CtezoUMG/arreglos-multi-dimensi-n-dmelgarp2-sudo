c9ad477a6791f822c5d900c47afe508facde895a
dmelgarp2-sudo <dmelgarp2@miumg.edu.gt>
2026-02-24 19:20:23 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,6 +18,13 @@ for (int i = 0; i < n; i++)
 // Instrucción: El alumno debe encontrar el valor máximo del arreglo 'numeros'
 int maximo = numeros[0]; 
 
+for (int i = 1; i < numeros.Length; i++)
+{
+    if (numeros[i] > maximo)
+    {
+        maximo = numeros[i];
+    }
+}
 // TODO: Implementar el algoritmo de búsqueda aquí
 
 
